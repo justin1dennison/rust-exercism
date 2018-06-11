d7d0e8c8caf9c5ec4b7293c82cb5ae3d2d03a273
justindevpro <justin@itpro.tv>
2018-06-11 16:30:14 -0400

    learned about the any method for iterators that will allow the reduction to a boolean based on closure

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,10 +1,5 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-        (1..limit)
-         .filter(|n| factors
-                        .iter()
-                        .map(|factor| n % factor == 0)
-                        .fold(false, |acc, x| acc || x))
-         .sum()
+    (1..limit)
+        .filter(|n| factors.iter().any(|factor| n % factor == 0))
+        .sum()
 }
-
-
