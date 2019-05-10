5ed86da6c2410292e07863837009bdabc0171afb
Justin Dennison <justin1dennison@gmail.com>
2019-05-10 06:33:07 -0400

    simplified using seconds

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -6,24 +6,20 @@ pub struct Clock {
     minutes: i32,
 }
 
+const SECONDS_PER_DAY: i32 = 86400;
+const SECONDS_PER_HOUR: i32 = 3600;
+const SECONDS_PER_MINUTE: i32 = 60;
+
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        Clock::from_minutes(minutes + 60 * hours)
+        Clock::from_seconds(SECONDS_PER_HOUR * hours +  SECONDS_PER_MINUTE * minutes)
     }
 
-    fn from_minutes(minutes: i32) -> Self {
-        let hs =  minutes / 60 % 24;
-        let ms = minutes % 60;
-        let (hours, minutes) = if minutes < 0 {
-            let hours = 24 + hs - 1;
-            let minutes = 60 + ms;
-            (hours + minutes / 60, minutes % 60)
-        } else {
-            let hours = (24 + hs) % 24;
-            let minutes = (60 + ms) % 60;
-            (hours + minutes / 60, minutes % 60)
-        };
-        Clock { hours, minutes }
+    fn from_seconds(seconds: i32) -> Self {
+       let seconds = (seconds % SECONDS_PER_DAY + SECONDS_PER_DAY) % SECONDS_PER_DAY;
+       let hours = seconds / SECONDS_PER_HOUR;
+       let minutes = (seconds % SECONDS_PER_HOUR) / SECONDS_PER_MINUTE;
+       Clock { hours, minutes }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
