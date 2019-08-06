e7ef5bdac6c8095f26a1c57762973b973ed0f3e9
Justin Dennison <justin1dennison@gmail.com>
2019-08-06 16:00:22 -0400

    used iterators to clean up

--- atbash-cipher/src/lib.rs
+++ atbash-cipher/src/lib.rs
@@ -1,66 +1,50 @@
-use std::str;
 
-enum CharPosition {
-    Found(usize),
-    NotFound,
-}
-pub const ALPHABET: &'static str = "abcdefghijklmnopqrstuvwxyz";
+pub const ALPHABET: &str = "abcdefghijklmnopqrstuvwxyz";
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
-    let plain = plain
-        .to_lowercase()
-        .replace(" ", "")
-        .replace(".", "")
-        .replace("?", "")
-        .replace(",", "");
-    let mut result = String::new();
     let reversed: String = ALPHABET.chars().rev().collect();
-    for character in plain.chars() {
-        if character.is_ascii() {
-            let position = match ALPHABET.find(character) {
-                Some(n) => CharPosition::Found(n),
-                None => CharPosition::NotFound,
-            };
-            if let CharPosition::Found(position) = position {
-                let cipher = match reversed.get(position..position + 1) {
-                    Some(s) => s,
-                    None => "",
-                };
-                result.push_str(cipher);
-            } else {
-                result.push_str(&character.to_string());
-            };
-        }
-    }
-    result
-        .as_bytes()
+    plain
+        .to_ascii_lowercase()
+        .chars()
+        .filter(|c| c.is_ascii_alphanumeric())
+        .map(|character| {
+            ALPHABET
+                .find(character)
+                .and_then(|n| reversed.get(n..=n).map(|s| s.to_string()))
+                .or_else(|| Some(character.to_string()))
+
+        })
+        .filter_map(|e| e)
+        .collect::<Vec<String>>()
         .chunks(5)
-        .map(str::from_utf8)
-        .collect::<Result<Vec<&str>, _>>()
-        .unwrap()
+        .map(|chunk| {
+            chunk
+                .iter()
+                .map(|s| s.to_string())
+                .collect::<Vec<_>>()
+                .join("")
+        })
+        .collect::<Vec<String>>()
         .join(" ")
         .to_string()
 }
 
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
-    let cipher = cipher.replace(" ", "");
-    let mut result = String::new();
     let reversed = ALPHABET.chars().rev().collect::<String>();
-    for character in cipher.chars() {
-        let position = match reversed.find(character) {
-            Some(n) => CharPosition::Found(n),
-            None => CharPosition::NotFound,
-        };
-        if let CharPosition::Found(n) = position {
-            let letter = match ALPHABET.get(n..n + 1) {
-                Some(found) => found,
-                None => "",
-            };
-            result.push_str(letter);
-        } else {
-            result.push_str(&character.to_string());
-        }
-    }
-    result
+    cipher
+        .to_ascii_lowercase()
+        .chars()
+        .filter(|c| c.is_ascii_alphanumeric())
+        .map(|character| {
+            reversed
+                .find(character)
+                .and_then(|n| ALPHABET.get(n..=n).map(|s| s.to_string()))
+                .or_else(|| Some(character.to_string()))
+
+        })
+        .filter_map(|e| e)
+        .collect::<Vec<String>>()
+        .join("")
+        .to_string()
 }
