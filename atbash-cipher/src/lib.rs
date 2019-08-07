5095eb735379161c3daf0efefb34c50c6427a3de
Justin Dennison <justin1dennison@gmail.com>
2019-08-07 13:05:27 -0400

    refactor to reduce duplication

--- atbash-cipher/src/lib.rs
+++ atbash-cipher/src/lib.rs
@@ -1,21 +1,29 @@
+#[macro_use]
+extern crate lazy_static;
 
 pub const ALPHABET: &str = "abcdefghijklmnopqrstuvwxyz";
-/// "Encipher" with the Atbash cipher.
-pub fn encode(plain: &str) -> String {
-    let reversed: String = ALPHABET.chars().rev().collect();
-    plain
-        .to_ascii_lowercase()
+
+lazy_static! {
+    static ref REVERSED: String = ALPHABET.chars().rev().collect();
+}
+
+fn transform(text: &str, domain: &str, range: &str) -> Vec<String> {
+    text.to_ascii_lowercase()
         .chars()
         .filter(|c| c.is_ascii_alphanumeric())
         .map(|character| {
-            ALPHABET
+            domain
                 .find(character)
-                .and_then(|n| reversed.get(n..=n).map(|s| s.to_string()))
+                .and_then(|n| range.get(n..=n).map(|s| s.to_string()))
                 .or_else(|| Some(character.to_string()))
-
         })
         .filter_map(|e| e)
         .collect::<Vec<String>>()
+}
+
+/// "Encipher" with the Atbash cipher.
+pub fn encode(plain: &str) -> String {
+    transform(plain, ALPHABET, &REVERSED)
         .chunks(5)
         .map(|chunk| {
             chunk
@@ -31,20 +39,5 @@ pub fn encode(plain: &str) -> String {
 
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
-    let reversed = ALPHABET.chars().rev().collect::<String>();
-    cipher
-        .to_ascii_lowercase()
-        .chars()
-        .filter(|c| c.is_ascii_alphanumeric())
-        .map(|character| {
-            reversed
-                .find(character)
-                .and_then(|n| ALPHABET.get(n..=n).map(|s| s.to_string()))
-                .or_else(|| Some(character.to_string()))
-
-        })
-        .filter_map(|e| e)
-        .collect::<Vec<String>>()
-        .join("")
-        .to_string()
+    transform(cipher, &REVERSED, ALPHABET).join("").to_string()
 }
