b9eb0ebc657a331ea6ee54092e3e146a10a3bf8e
Justin Dennison <justin1dennison@gmail.com>
2019-08-08 07:24:08 -0400

    refactor to simplify closures and extract functions

--- atbash-cipher/src/lib.rs
+++ atbash-cipher/src/lib.rs
@@ -1,43 +1,43 @@
 #[macro_use]
 extern crate lazy_static;
 
-pub const ALPHABET: &str = "abcdefghijklmnopqrstuvwxyz";
+pub const LETTERS: &str = "abcdefghijklmnopqrstuvwxyz";
 
 lazy_static! {
-    static ref REVERSED: String = ALPHABET.chars().rev().collect();
+    static ref REVERSED: Vec<char> = LETTERS.chars().rev().collect::<Vec<char>>();
+    static ref ALPHABET: Vec<char> = LETTERS.chars().collect::<Vec<char>>();
 }
 
-fn transform(text: &str, domain: &str, range: &str) -> Vec<String> {
+fn transform(text: &str, domain: Vec<char>, range: Vec<char>) -> Vec<String> {
+    let lookup = |character: char| {
+        domain
+            .iter()
+            .position(|&x| x == character)
+            .and_then(|n| Some(range[n].to_string()))
+            .or_else(|| Some(character.to_string()))
+            .unwrap()
+    };
     text.to_ascii_lowercase()
         .chars()
         .filter(|c| c.is_ascii_alphanumeric())
-        .map(|character| {
-            domain
-                .find(character)
-                .and_then(|n| range.get(n..=n).map(|s| s.to_string()))
-                .or_else(|| Some(character.to_string()))
-        })
-        .filter_map(|e| e)
+        .map(lookup)
         .collect::<Vec<String>>()
+}
+
+fn chunk_to_string(chunk: &[String]) -> String {
+    chunk.iter().map(|s| s.to_string()).collect::<String>()
 }
 
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
-    transform(plain, ALPHABET, &REVERSED)
+    transform(plain, ALPHABET.to_vec(), REVERSED.to_vec())
         .chunks(5)
-        .map(|chunk| {
-            chunk
-                .iter()
-                .map(|s| s.to_string())
-                .collect::<Vec<_>>()
-                .join("")
-        })
+        .map(chunk_to_string)
         .collect::<Vec<String>>()
         .join(" ")
-        .to_string()
 }
 
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
-    transform(cipher, &REVERSED, ALPHABET).join("").to_string()
+    transform(cipher, REVERSED.to_vec(), ALPHABET.to_vec()).join("")
 }
