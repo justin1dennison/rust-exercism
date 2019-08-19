ba1703ce90a617d7142fe43ad3cf785472289997
Justin Dennison <justin1dennison@gmail.com>
2019-08-19 07:11:26 -0400

    changed to using collect instead of from iter. Additionally, removed the redundant checks.

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -1,5 +1,4 @@
 use std::collections::HashSet;
-use std::iter::FromIterator;
 
 
 fn is_anagram<'a>(fst: &'a str, snd: &'a str) -> bool {
@@ -7,19 +6,14 @@ fn is_anagram<'a>(fst: &'a str, snd: &'a str) -> bool {
     let mut snd_letters = snd.chars().collect::<Vec<char>>();
     fst_letters.sort();
     snd_letters.sort();
-    let same_letters = fst_letters
-        .iter()
-        .zip(snd_letters.iter())
-        .fold(true, |acc, (l, r)| acc && l == r);
-    (fst_letters == snd_letters &&
-     fst.len() == snd.len() &&
-     same_letters && 
-     fst != snd)
+    fst_letters == snd_letters && fst != snd
 }
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
-    let anagrams = possible_anagrams
+    possible_anagrams
         .iter()
-        .filter(|w| is_anagram(&word.to_lowercase(), &w.to_lowercase()));
-    HashSet::from_iter(anagrams.cloned())
+        .filter(|w| is_anagram(&word.to_lowercase(), &w.to_lowercase()))
+        .copied()
+        .collect::<HashSet<&'a str>>()
+
 }
