5e75580f46d096b1070c72e188e31b091d13fc89
Justin Dennison <justin1dennison@gmail.com>
2019-09-10 15:29:43 -0400

    reorganized sublist to consolidate sublist and superlist check functions

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -8,20 +8,16 @@ pub enum Comparison {
 
 fn is_sublist<T: PartialEq>(fst: &[T], snd: &[T]) -> bool {
     let size: usize = fst.len();
-    snd.windows(size).any(|xs| xs == fst)
+    (fst.is_empty() && !snd.is_empty()) || snd.windows(size).any(|xs| xs == fst)
 }
 
 fn is_superlist<T: PartialEq>(fst: &[T], snd: &[T]) -> bool {
     let size: usize = snd.len();
-    fst.windows(size).any(|xs| xs == snd)
+    (!fst.is_empty() && snd.is_empty()) || fst.windows(size).any(|xs| xs == snd)
 }
 
 pub fn sublist<T: PartialEq>(fst: &[T], snd: &[T]) -> Comparison {
-    if fst.is_empty() && !snd.is_empty() {
-        Comparison::Sublist
-    } else if !fst.is_empty() && snd.is_empty() {
-        Comparison::Superlist
-    } else if fst == snd {
+    if fst == snd {
         Comparison::Equal
     } else if is_sublist(fst, snd) {
         Comparison::Sublist
